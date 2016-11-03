a73387f289f15ae24636c87ba7a16650c12d0f89
unknown <ch0125@GARAIO-CH3.garaio.com>
2016-11-03 15:06:38 +0100

    Tryouts Lesson 2

---
+++ Tooling & Datamanagement/lesson2.R
@@ -0,0 +1,39 @@
+#12.3.2
+
+#2
+x <- c(rep(1:5, 10))
+x
+
+#3
+y <- c(x[12],x[20],x[50])
+y
+
+y2 <- x[c(12, 20, 50)]
+y2
+
+identical(c(x[12],x[20],x[50]), x[c(12, 20, 50)])
+
+#4
+freunde <- c("Herbert", "Stefan", "Roland")
+freunde
+
+#identical(c(rep(1:5, 10)),rep(1:5, 10))
+x <- c(a=10, b=20, c=30)
+cbind(x,x)
+rbind(x,x)
+
+x = 1:100
+x
+
+y = x[0:10]
+y
+
+z = x[91:100]
+z
+
+cbind(y,z)
+
+class(cbind(y,z))
+
+x.sub <- x[x<11 | x >91]
+x.sub
