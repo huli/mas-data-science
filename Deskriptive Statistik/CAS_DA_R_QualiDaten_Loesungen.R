b999dfca876308ca01a024b20bf64a8bd332db88
Huli <huli@eitschei.com>
2016-11-04 14:20:52 +0100

    Exercise completed

--- Deskriptive Statistik/CAS_DA_R_QualiDaten_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_QualiDaten_Loesungen.R
@@ -73,19 +73,21 @@ composition.max.mean
 #Aufgabe 2: Gruppenstatistik
 # ----------------------------------------------------------------------------------------------------
 
-# Maler mit Color groesser oder gleich 14
+# Alle Maler mit Color >= 14
 painters.color.high <- painters[painters$Colour >= 14,]
 painters.color.high
-
-# Maler mit Color kleiner als 14
-painters.color.low <- painters[painters$Colour < 14, ]
-painters.color.low
 
 # Anteil von Malern mit Color >= 14
 options.old <- options(digits = 4)
 
+# Anteil von Malern mit Color >= 14 in Prozenten
 painters.color.high.percentage <- 100 * nrow(painters.color.high) / nrow(painters)
 painters.color.high.percentage
 
+# Das Ganze noch mit Data Frame zum Ablesen/Kontrolle 
+painters.extended <- painters
+painters.extended["High Color"] <- as.numeric(painters.extended$Colour >= 14)
+painters.color.table <- table(rownames(painters.extended), painters.extended$`High Color`)
+addmargins(prop.table(painters.color.table)) * 100
 
 
