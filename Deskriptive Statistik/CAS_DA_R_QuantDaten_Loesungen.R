c61c3d67ca06f5e5f9fd16bc03cbcb4cadde7383
Huli <huli@eitschei.com>
2016-11-11 08:35:40 +0100

    Histogram added.

--- Deskriptive Statistik/CAS_DA_R_QuantDaten_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_QuantDaten_Loesungen.R
@@ -4,3 +4,22 @@
 # Datum:      11.11.2016
 # Aufgaben:   CAS_DA_R_QuantDaten_Aufgaben.pdf
 # ----------------------------------------------------------------------------------------------------
+
+
+# Aufgabe: Häufigkeitsverteilung quantitativer Daten
+# ----------------------------------------------------------------------------------------------------
+
+# Haufigkeitsverteilung ohne Gruppierung
+table(faithful$waiting)
+
+# Intervall mit meisten Eruptionen
+waiting.max <- sort(table(faithful$waiting), decreasing = TRUE)[1]
+waiting.max
+
+# Range von Wartezeiten bestimmen
+range.waiting <- range(faithful$waiting)
+
+# Haeufigkeitsverteilung der Eruptionszeiten
+hist(faithful$eruptions)
+
+
