8c46b443eff0dbe25240ffb474c6badb5113a192
Huli <huli@eitschei.com>
2016-11-11 07:59:55 +0100

    File added and clean ups.

--- Deskriptive Statistik/CAS_DA_R_2dimVerteilungen_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_2dimVerteilungen_Loesungen.R
@@ -12,20 +12,31 @@ load("C:\\Users\\ch0125\\Dropbox\\BFH\\Deskriptive Statistik\\Daten_WachstumX.RD
 attach(Daten_Wachstum)
 
 # Aufgabe: zweidimensionale Haeufigkeitsverteilung
+# ----------------------------------------------------------------------------------------------------
 
 gender.motiv.freq <- table(Geschlecht, Motiv)
 gender.motiv.freq
 
+
 # Aufgabe: Randverteilungen
+# ----------------------------------------------------------------------------------------------------
 addmargins(gender.motiv.freq)
 
+
 # Aufgabe: Relative Zweidimensionale Verteilung
+# ----------------------------------------------------------------------------------------------------
 gender.motiv.relfreq <- prop.table(table(Geschlecht, Motiv))
 gender.motiv.relfreq
 
+
 # Aufgabe: Bedingte Verteilung 1
+# ----------------------------------------------------------------------------------------------------
+
 addmargins(prop.table(gender.motiv.freq, 1))
 
+
 # Aufgabe: Bedingte Verteilung 2
+# ----------------------------------------------------------------------------------------------------
+
 addmargins(prop.table(gender.motiv.freq, 2))
 
