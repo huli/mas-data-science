2c3680b056475ad7e0438d37ad0ec2da21b02fee
Huli <huli@eitschei.com>
2016-11-11 09:51:50 +0100

    Exercise completed.

--- Deskriptive Statistik/CAS_DA_R_QuantDaten_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_QuantDaten_Loesungen.R
@@ -16,10 +16,97 @@ table(faithful$waiting)
 waiting.max <- sort(table(faithful$waiting), decreasing = TRUE)[1]
 waiting.max
 
-# Range von Wartezeiten bestimmen
-range.waiting <- range(faithful$waiting)
+# Range von Eruptionen bestimmen
+eruptions.range <- range(faithful$eruptions)
 
-# Haeufigkeitsverteilung der Eruptionszeiten
-hist(faithful$eruptions)
+# Haeufigkeitsverteilung der Eruptionszeiten (mit Defaults)
+hist(faithful$eruptions, right = FALSE)
+
+# Haeufigkeitsverteilung der Eruptionszeiten (ohne Defaults)
+hist(faithful$eruptions, right = FALSE, breaks =  seq(1.5, 5.2, by = .2),
+     xlab = "duration of eruption in seconds", ylab = "number of eruptions",
+     main = "Frequency distribution of eruption durations",
+     col = rainbow(18))
+
+
+# Aufgabe: Histogramm
+# ----------------------------------------------------------------------------------------------------
+
+hist(faithful$waiting, right = FALSE,
+     main = "Waiting times of old faithful", xlab = "waiting duration in seconds", ylab = "eruptions", 
+     col = rainbow(13))
+
+
+# Aufgabe: Relative Häufigkeitsverteilung stetiger Daten
+# ----------------------------------------------------------------------------------------------------
+
+# Ungruppierte Vergleiche
+prop.table(table(faithful$waiting))
+
+# Range von Wartezeigen bestimmen
+waiting.range <- range(faithful$waiting)
+
+# Gruppenbrueche bestimmen
+waiting.breaks <- seq(40, 100, by = 5)
+
+# Effektive Wartezeiten in Gruppen verteilen
+waitings.cuts <- cut(faithful$waiting, waiting.breaks, right = FALSE)
+waitings.freq <- table(waitings.cuts)
+waitings.relfreq <- prop.table(waitings.freq)
+
+# Relative Haeufigkeitsverteilung gruppiert
+waitings.relfreq
+
+# Gerundet und in Prozenten
+waitings.percentage <- round(waitings.relfreq, digits = 4) * 100
+
+# Und noch absolute Haeufigkeiten und relative Haeufigeiten gegenuebergestellt
+cbind(waitings.freq, waitings.percentage)
+
+
+# Aufgabe: Kumulierte Häufigkeitsverteilung
+# ----------------------------------------------------------------------------------------------------
+
+# Summierte absolute Haeufigkeitsverteilung
+waitings.freq.cum <- cumsum(waitings.freq)
+
+# Und noch als Spalten
+cbind(waitings.freq.cum)
+
+
+# Aufgabe: Kumulierte Häufigkeitsverteilungskurve
+# ----------------------------------------------------------------------------------------------------
+
+# Absolute, kumulierte Haeufigkeitsverteilung plotten
+plot(waiting.breaks, waitings.freq.cum0,
+     main = "old faithful eruptions",
+     xlab = "cumulative waiting frequency", ylab = "waiting duration in seconds"
+)
+
+# Kurve hinzufuegen
+lines(waiting.breaks, waitings.freq.cum0)
+
+
+# Aufgabe: Kumulierte relative Häufigkeitsverteilung
+# ----------------------------------------------------------------------------------------------------
+
+# Zu diese Zweck brauchen wir die relativen Zahlen
+waitings.relfreq.cum <- cumsum(waitings.relfreq)
+
+# 0-Punkt hinzufuegen
+waitings.relfreq.cum0 <- c(0, waitings.relfreq.cum)
+
+# Kummulierte Haeufigkeitsverteilung plotten
+plot(waiting.breaks, waitings.relfreq.cum0, 
+     main = "old faithful eruptions",
+     xlab = "cumulative waiting proportion", ylab = "waiting duration in seconds"
+)
+
+
+# Aufgabe: Kumulierte relative Häufigkeitsverteilungskurve
+# ----------------------------------------------------------------------------------------------------
+
+# Kurve hinzufuegen
+lines(waiting.breaks, waitings.relfreq.cum0)
 
 
