c32185834e58d7f9bc40ae06bed66ede5999722b
Huli <huli@eitschei.com>
2016-11-17 13:55:03 +0100

    File changed.

---
+++ Tooling & Datamanagement/lesson4.R
@@ -0,0 +1,46 @@
+
+# Damit dta verstanden wird
+library(foreign)
+
+titanic <- read.dta("http://www.stata-press.com/data/kkd/titanic2.dta")
+
+# Daten kennenernen
+head(titanic)
+summary(titanic)
+
+# Vergelich von subset und über Indizes
+x <- titanic[titanic$survived == "yes", "age2"]
+y <- subset(titanic, survived == "yes")$age2
+identical(x, y)
+
+# Kreuztablle
+addmargins(round(prop.table(table(titanic$class, titanic$survived)), 4))
+
+# Median fuer Alter von Gestorbenen
+people.survived <- subset(titanic, survived == "yes")
+people.dead <- subset(titanic, survived == "no")
+
+# Mittelwert bestimmen
+mean(people.dead$age2)
+mean(people.survived$age2)
+
+# Median bestimmen
+median(people.dead$age2)
+median(people.survived$age2)
+
+t.test(people.dead$age2, people.survived$age2)
+
+# Mit tapply (tapply(werte, gruppierung, funktion))
+tapply(titanic$age2, titanic$survived, mean)
+tapply(titanic$age2, titanic$survived, median)
+
+# Scope binden (sollte nicht verwendet werden)
+attach(titanic)
+median(age2)
+detach(titanic)
+
+# Besatzung entfernen welches nicht verwendet wird
+titanic$class <-droplevels(titanic$class)
+
+
+
