b6d96023af2d79924046c05a3010b2881ba6720a
Huli <huli@eitschei.com>
2016-11-17 16:07:14 +0100

    Some corrections.

--- Tooling & Datamanagement/exercise_dplyr.R
+++ Tooling & Datamanagement/exercise_dplyr.R
@@ -11,7 +11,8 @@ input.data <- read.dta("http://www.farys.org/daten/allbus2008.dta")
 clean.data <- 
   input.data %>% 
   select("Geschlecht" = v151, "Alter" = v154, "Einkommen" = v386) %>% 
-  filter(Alter %notin% c(99997, 99998, 99999, 999)) %>% 
+  #filter(!Alter %notin% c(99997, 99998, 99999, 999)) %>% 
+  filter(Alter < 999 & Einkommen < 99997) %>% 
   group_by(Alter, Geschlecht) %>% 
   summarise(Durchschnittseinkommen = mean(Einkommen))
 
@@ -20,3 +21,4 @@ library(ggplot2)
 
 ggplot(clean.data, aes(x=Alter, y=Durchschnittseinkommen, color=Geschlecht)) +
   geom_line()
+
