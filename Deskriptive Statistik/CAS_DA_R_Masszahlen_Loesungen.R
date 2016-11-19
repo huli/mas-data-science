ba100b96b648ee7669800d68a4428e4fe4a4ca15
Huli <huli@eitschei.com>
2016-11-19 09:13:38 +0100

    Exercise completed.

--- Deskriptive Statistik/CAS_DA_R_Masszahlen_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_Masszahlen_Loesungen.R
@@ -49,4 +49,59 @@ stat.span(faithful$waiting)
 # Aufgabe: Interquartilsabstand
 # ----------------------------------------------------------------------------------------------------
 
+IQR(faithful$waiting)
+
+
+# Aufgabe: Boxplot
+# ----------------------------------------------------------------------------------------------------
+
+boxplot(faithful$waiting, 
+        horizontal = TRUE,
+        main = "Boxplot of faithful waitings", col = "bisque")
+
+
+# Aufgabe: Varianz
+# ----------------------------------------------------------------------------------------------------
+
+# Stichprobenvarianz
+var(faithful$waiting)
+
+# Populationsvarianz
+var.pop <- function(x){
+  stopifnot(class(x) == "numeric")
+  var(x) * (length(x)-1) / length(x)
+}
+
+var.pop(faithful$waiting)
+
+
+# Aufgabe: Standardabweichung
+# ----------------------------------------------------------------------------------------------------
+
+# Hinweis: Die Aufgabe heissst Standardabweichung, jedoch sollen noch einmal beide Varianzen
+# bestimmt werden. Ich gehe davon aus, hier müssen korrekterweise beide Standardabweichungen ausgegeben
+# werden.
+
+# Stichprobenstandardabweichung
+sd(faithful$waiting)
+
+# Populationsstandardabweichung
+sd.pop <- function(x){
+  stopifnot(class(x) == "numeric")
+  sqrt(var.pop(x))
+}
+
+sd.pop(faithful$waiting)
+
+
+# Aufgabe: Korrelationskoeffizient
+# ----------------------------------------------------------------------------------------------------
+
+# Korrelationskoeffizient nach Pearson (wäre auch der Default)
+cor(swiss$Fertility, swiss$Education, 
+    method = "pearson")
+
+# Interpretation: Das heisst, die Korrelation ist stark und gegensinnig
+# (Siehe plot(swiss$Fertility, swiss$Education))
+
 
