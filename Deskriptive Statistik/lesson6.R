d3c5bc270e451391de474c8bfb97c6aad00ed042
Huli <huli@eitschei.com>
2016-12-01 10:39:58 +0100

    Content added.

--- Deskriptive Statistik/lesson6.R
+++ Deskriptive Statistik/lesson6.R
@@ -52,3 +52,78 @@ yprob <- dpois(0:20, lambda = 12)
 names(yprob) <- 0:20
 barplot(yprob)
 
+# Wieso sind beide Balken gleich hoch (11, 12)
+dpois(12, 12)
+dpois(11, 12)
+
+# -> nobody knows
+
+# Stetige Modelle
+# -------------------------------------------------------------------------------------------
+
+# Wir wechseln von Säulenhöhe zu Fläche und somit Dichte
+
+# Beispiel 21: Funktionsdauer von Taschenrechnern
+# Die Funktionsdauer x ist normalverteilt mit Erwartungswert m = 120 h und
+# theoretischer Varianz s2 = 100.
+# Wie wahrscheinlich ist es, dass die Funktionsdauer
+
+# a) höchstens 135 h
+# von bis in R heisst mit p beginnen (d = dichte)
+pnorm(135, mean = 120, sd = 10)
+
+# b) mehr als 135 h
+pnorm(135, mean = 120, sd = 10, lower.tail = FALSE)
+
+# stetige Gleichverteilung
+# Alle Wahrscheinlichkeiten sind gleich Wahrscheinlich
+# (Warten auf den Bus, Zufallszahlen -> horizontale Linie)
+
+xv <- seq(0, 5,length=100)
+plot(xv, dunif(xv, min = 1, max = 3), type = "l", ylab = "Uni(x)", xlab = "x")
+
+# für Fläche p
+punif(2.5, 1, 3)
+
+?punif
+
+# r für Random, d für density, q für quantile
+runif(10, min = 1, max = 3)
+
+# Wieviel Zeit vergeht bis?
+# Exponentialverteilung (das Pendant zur Poissonverteilung)
+# (Anzahl Pizzas pro Zeit wäre Poission, Zeit bis Pizza ist Exponential)
+# (Lebensdauer von Bauteilen ohne Altererscheinung, Lebensdauer von Atomen,
+# Anzahl Zeit zwischen Versicherungsfällen)
+
+#  Ereignis tritt einmal pro Minute auf
+xv <- seq(0,5,length=100)
+plot(xv, dexp(xv, rate=1), type = "l", ylab = "Exp(x)",
+     xlab = "x")
+
+# Problem: Die durchschnittliche Abfertigungszeit an der Kasse eines
+# Supermarktes betrage 3 Minuten. Mit welcher Wahrscheinlichkeit wird
+# ein Kunde in weniger als 2 Minuten bedient?
+
+# Zeit die vergeht bis -> Exontentialverteilung
+pexp(2, rate = 1/3)
+
+# Wie lange müssten wir warten, bis die Wahrscheinlichkeit 50 % ist
+# Wert auf x-Achse suchen ist immer q..
+qexp(.5, rate = 1/3)
+
+
+# Normalverteilung
+# Alle Dinge die aus vielen Dingen zustande kommen sind Normalverteilt
+# Bei allen Verteilungen kann ich mit einer hohen Anzahl Wiederholungen
+# auf eine Normalverteilung kommen (Ausnahme Gleichverteilung)
+# (Normalverteilung heisst auch immer Zufall)
+xv <- seq(-5,5, length = 200)
+plot(xv, dnorm(xv), type = "l")
+
+
+
+
+
+
+
