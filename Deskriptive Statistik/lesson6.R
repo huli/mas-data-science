9383947c9ff05b68516f16b061736e1ec31923cb
Huli <huli@eitschei.com>
2016-12-01 09:12:53 +0100

    Lesson 6 added.

---
+++ Deskriptive Statistik/lesson6.R
@@ -0,0 +1,54 @@
+
+# Binominalverteilung
+# -------------------------------------------------------------------------------------------
+
+# Wahrscheinlichkeit bleibt immer gleich
+yprob <- dbinom(0:12, size=length(0:12)-1, prob = 1/5)
+names(yprob)  <- 0:12
+barplot(yprob)
+
+
+# Hypergeometrische Verteilung
+# -------------------------------------------------------------------------------------------
+# Wahrscheinlichkeit verändert sich 
+# (Befrage Person wird nicht mehr befragt, M&M's wird gegessen, Lotto)
+# Problem: Beim Schweizer Zahlenlotto sind 6 Zahlen aus 42 zu ziehen.
+# Wir bezeichnen mit x die Anzahl der richtig angekreutzten Zahlen.
+# Bestimmen Sie die Wahrscheinlichkeitsverteilung und stellen Sie diese
+# grafisch dar
+
+# Achtung: 0:6 (entspricht 7, da man auch 0 Fragen richtig haben kann)
+yprob <-dhyper(0:6, m=6, n=36, k=6) 
+names(yprob) <- 0:6
+barplot(yprob)
+
+# Fall aus den Folien (Beispiel 18: Rechnen mit Wahrscheinlichkeiten (Urnenmodell ohne Z.))
+yprob <- dhyper(0:3, m=4, n=6, k=3)
+names(yprob) <- 0:3
+barplot(yprob)
+
+# Poissionverteilung
+# Näherungsmodell für Wahrscheinlichkeiten pro Zeit (Grenzfall des Binomialmodells)
+# -------------------------------------------------------------------------------------------
+
+# Man will unendlich fein unterteilen damit man noch auf ein Ja/Nein-Modell kommt (Bernouille-Prozess)
+
+# Problem: Eine Brücke wird durchschnittlich von 12 Autos pro Minute
+# passiert. Wie gross ist die Wahrscheinlichkeit, dass sich in einer
+# Minute mehr als 17 Autos auf der Brücke befinden?
+
+# Eine Achse ist die Zeit und diese ist beliebig fein unterteilbar
+ppois(16, lambda = 12) # lower tail (Wahrscheinlichkeit für 16 Autos und weniger)
+
+# die Wahrscheinlichkeit das mehr als 16 Autos
+1-ppois(16, lambda = 12)
+
+# oder auch 
+ppois(16, lambda = 12, lower.tail = FALSE)
+
+# Aufgabe wir wissen es kommen im Schnitt pro Minute 12
+# wir wollen sehen wie gross die Wahrscheinlichkeit für 20 Autos
+yprob <- dpois(0:20, lambda = 12)
+names(yprob) <- 0:20
+barplot(yprob)
+
