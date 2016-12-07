ad699960dd5539797ba4f15aa8863dd00e866e68
Huli <huli@eitschei.com>
2016-12-07 12:05:13 +0100

    Boxplot turned because of specification.

--- Tooling & Datamanagement/Ebay/code.R
+++ Tooling & Datamanagement/Ebay/code.R
@@ -11,25 +11,26 @@ ebay_data %>%
     mutate(makellos = rating > .98) %>% 
     mutate(geraetetyp = str_match(subcat, "[A-Za-z0-9]*\\s[A-Za-z0-9]*")[,1]) -> ebay_clean
 
+opar <- par()      # make a copy of current settings
+
 boxplot(price ~ geraetetyp, data = ebay_clean,
         boxwex = 0.35,
-        at = 1:7,
-        horizontal = TRUE,
         frame = FALSE,
         notch = TRUE,
         main = "Preise von Mobiltelefonen auf Ebay", 
         xlab = "Preise in $",
-        subset = makellos, col = "gold", las = 1)
+        subset = makellos, col = "darkgreen",
+        cex.axis = .8)
+
 
 boxplot(price ~ geraetetyp, data = ebay_clean,
         boxwex = 0.35,
         at = 1:7 -.4,
-        horizontal = TRUE,
         notch = TRUE,
         frame = FALSE,
         add = TRUE,
-        subset = !makellos, col = "darkgreen", names = rep("",7))
+        subset = !makellos, col = "red", axes = FALSE)
 
 legend("topright", c("makellos", "nicht makellos"),
        inset = .01,
-       fill = c("gold", "darkgreen"))
+       fill = c("darkgreen", "red"))
