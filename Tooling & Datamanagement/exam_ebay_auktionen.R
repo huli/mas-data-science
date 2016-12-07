8db43c48cfa1025fb6b1d290a6b94d3e918f2f8d
Huli <huli@eitschei.com>
2016-12-07 09:41:44 +0100

    Boxplot added.

--- Tooling & Datamanagement/exam_ebay_auktionen.R
+++ Tooling & Datamanagement/exam_ebay_auktionen.R
@@ -9,23 +9,24 @@ ebay_data %>%
     filter(sepos >= 12) %>% 
     mutate(rating = sepos/(sepos + seneg)) %>% 
     mutate(makellos = rating > .98) %>% 
-    mutate(geraetetyp = str_match(subcat, "\\A(\\w+)")[,1]) -> ebay_clean
+    mutate(geraetetyp = str_match(subcat, "\\w+")[,1]) -> ebay_clean
 
 
-str_match("Samsung E700", "\\A(\\w+)")[,1]
+boxplot(price ~ geraetetyp, data = ebay_clean,
+        boxwex = 0.35,
+        at = 1:4 - 0.3,
+        notch = TRUE,
+        main = "Preise von Mobiltelefonen auf Ebay",        
+        xlab = "Gerätetypen",
+        ylab = "Preise in $",
+        subset = makellos, col = "gold")
 
-boxplot(ebay_clean$price ~ ebay_clean$geraetetyp, ebay_clean)
+boxplot(price ~ geraetetyp, data = ebay_clean,
+        boxwex = 0.35,
+        at = 1:4 + .1,
+        notch = TRUE,
+        add = TRUE,
+        subset = !makellos, col = "darkgreen", names = c("","",""))
 
-
-boxplot(len ~ dose, data = ToothGrowth,
-        boxwex = 0.25, at = 1:3 - 0.2,
-        subset = supp == "VC", col = "yellow",
-        main = "Guinea Pigs' Tooth Growth",
-        xlab = "Vitamin C dose mg",
-        ylab = "tooth length",
-        xlim = c(0.5, 3.5), ylim = c(0, 35), yaxs = "i")
-boxplot(len ~ dose, data = ToothGrowth, add = TRUE,
-        boxwex = 0.25, at = 1:3 + 0.2,
-        subset = supp == "OJ", col = "orange")
-legend(2, 9, c("Ascorbic acid", "Orange juice"),
-       fill = c("yellow", "orange"))
+legend(2, 9, c("makellos", "nicht makellos"),
+       fill = c("gold", "darkgreen"))
