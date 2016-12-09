7a19c63e3596c403ba3f577f1052d0db99ad7208
Huli <huli@eitschei.com>
2016-12-09 11:17:26 +0100

    Some more distributions.

--- Deskriptive Statistik/CAS_DA_R_Verteilungen_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_Verteilungen_Loesungen.R
@@ -14,6 +14,8 @@
 #   Kann man diese wirklich so Zusammenrechnen? Vermmutlich nicht, es müssten sich die 
 #   Wahrscheinlichkeiten ja mal rechnen, resp. verkleinern
 #   Aber es gibt ja verschiedene Varianten
+# Normalverteilung, letzte Frage
+# Chi-Quadrat, fehlt da nicht noch irgend eine Mittelwert oder so? Wie kann ich das intuitiv deuten
 
 
 
@@ -28,6 +30,7 @@ pbinom(2, size = 5, prob = 18/37, lower.tail = FALSE)
 
 # Welche Anzahl der Gewinne wird in 90% der Fälle höchstens
 # erreicht?
+
 qbinom(.9, size = 5, prob = 18/ 37)
 # 4
 
@@ -38,17 +41,21 @@ qbinom(.9, size = 5, prob = 18/ 37)
 # Hinweis: wir gehen hier davon aus, dass niemand das Restaurant auch verlässt?
 
 # Kurze Darstellung zur Vereinfachung
+
 plot(dpois(0:20, lambda = 12.1), type = "l")
 
 # Es sind genau 8 Gäste im Restaurant.
+
 dpois(8, lambda = 12.1) 
 # 0.06335767
 
 # Es sind höchstens 10 Gäste im Restaurant.
+
 ppois(10, lambda = 12.1)
 #  0.3368338
 
 # Es sind zwischen 9 und 15 Gäste im Restaurant.
+
 ppois(15, lambda = 12.1) - ppois(8, lambda = 12.1)
 # 0.6885025
 
@@ -61,12 +68,14 @@ ppois(10, lambda = 12.1, lower.tail = FALSE)
 
 # Welche Verteilung hat die Zufallsvariable X, welche die gesamte
 # Pendelzeit von Haustür bis ins Büro beschreibt?
+
 s <- seq(0, 40, length = 100)
 plot(s, dunif(s, 6+10, 6+5+15), type = "l")
 # Sie ist Gleichverteilt über den Minuten 16 bis 26 und
 
 # Mit welcher Wahrscheinlichkeit schaffen Sie es noch rechtzeitig
 # ins Büro?
+
 punif(20, 6+10, 6+5+15)
 # 0.4
 
@@ -91,4 +100,81 @@ max_wartezeit <- 4
 moegliche_wartezeiten <- seq(0, max_wartezeit, length = 10)
 plot(wartezeiten_bus, punif(moegliche_wartezeiten, 0, 5) * punif(max_wartezeit-moegliche_wartezeiten, 0, 5))
 
+
+# Aufgabe: Exponentialverteilung
+# ----------------------------------------------------------------------------------------------------
+
+# Welche Verteilung hat die Zufallsvariable X, welche die Dauer der
+# Telefongespräche in Minuten beschreibt?
+
+# Eine Exponentialverteilung
+
+
+# Das Telefon klingelt. Wie gross ist die Wahrscheinlichkeit, dass
+# dieses Gespräch höchstens eine Minute dauert?
+
+pexp(1, rate = 1/3)
+# 0.2834687
+
+# Wie gross ist die Wahrscheinlichkeit, dass das Gespräch länger
+# als eine Minute dauert?
+
+pexp(1, rate = 1/3, lower.tail = FALSE)
+# 0.7165313
+
+# Mit welcher Wahrscheinlichkeit dauert das Gespräch zwischen
+# einer und drei Minuten?
+
+pexp(3, rate = 1/3) - pexp(1, rate = 1/3)
+# 0.3486519
+
+# Berechnen und interpretieren Sie das 25%-Quantil dieser
+# Verteilung
+
+qexp(.25, rate = 1/3)
+# 0.8630462
+# Interpretation: 25% der Gespräche sind kürzer als 0.86 Minuten (ca 51 Sekunden) 
+
+
+# Aufgabe: Normalverteilung
+# ----------------------------------------------------------------------------------------------------
+
+# um weniger als 2 g vom Mittelwert abweicht?
+
+sd_chips <- 4
+mean_chips = 200
+pnorm(q = mean_chips + 2, mean = mean_chips, sd = sd_chips) -
+  pnorm(q = mean_chips - 2, mean = mean_chips, sd = sd_chips)
+# 0.3829249
+
+# über 205 g liegt?
+
+pnorm(205, mean = mean_chips, sd = sd_chips, lower.tail = FALSE)
+# 0.1056498
+
+# Welches Gewicht wird von 95% der Tüten überschritten?
+
+qnorm(.05, mean = mean_chips, sd = sd_chips)
+# 193.4206 (g)
+
+
+# Aufgabe: Chi-Quadrat-Verteilung
+# ----------------------------------------------------------------------------------------------------
+
+# Problem: Mit welcher Wahrscheinlichkeit liegt der Wert einer χ^2
+# Verteilung mit df = 11 über 15?
+
+pchisq(15, df = 11, lower.tail = FALSE)
+# 0.1824969
+
+
+# Aufgabe: Studentsche t-Verteilung
+# ----------------------------------------------------------------------------------------------------
+
+# Problem: Mit welcher Wahrscheinlichkeit liegt der Wert der
+# Studentschen t-Verteilung unter −0.5, respektive unter 1? Der
+# Freiheitsgrad sei 7.
+
+pt(c(-.5, 1), df = 7)
+#  0.3162036 0.8246917 (x < -0.5 :  0.3162036 und x < 1 = 0.8246917)
 
