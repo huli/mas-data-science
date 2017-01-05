0d46c19e83181660010f9679466b87b93aa1077b
Huli <huli@eitschei.com>
2017-01-05 18:16:44 +0100

    Home work finished. Without missing questions.

--- Deskriptive Statistik/CAS_DA_R_Tests_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_Tests_Loesungen.R
@@ -38,6 +38,10 @@ z <- qnorm(alpha, mean = 10000, sd = 120/sqrt(n))
 m0 < z
 ## [1] TRUE -> 0-Hypothese verwerfen
 
+# Antwort:
+# Bei einem Konfindenzlevel von 99% muss die Behauptung des Herstellers
+# von einer Mindestlebensdauer von 10'000 Stunden verworfen werden.
+
 
 # Aufgabe: Rechtsseitiger Test bei µ, σ bekannt
 # ----------------------------------------------------------------------------------------------------
@@ -60,6 +64,9 @@ z <- qnorm(1-alpha, sd = .25/sqrt(n), lower.tail = FALSE)
 z > m0 
 ## [1] FALSE -> 0-Hypothese kann behalten werden.
 
+# Antwort:
+# Bei einem Konfidenzlevel von 90% kann die Behauptung des Herstellers
+# von einem maximalen Anteil von 2g beibehalten werden.
 
 
 # Aufgabe: Zweiseitiger Test bei µ, σ bekannt
@@ -78,10 +85,86 @@ bulbs <- scan("C:\\temp\\lightbulbs.txt")
 t.test(bulbs, mu = 10000, conf.level = .99, alternative = "less")
 # p-Wert ist kleiner als 1% -> 0-Hypothese verwerfen
 
+# Antwort:
+# Nein, die Hypothese kann mit einem Signifikanzniveau von 1% nicht gehalten werden.
+# Der wahre Mittelwert der Grundgesamtheit ist mit 99% Konfidenz kleiner als 10'000 Stunden
 
 
 
 # Rechtsseitiger Test bei µ, σ unbekannt
 # ----------------------------------------------------------------------------------------------------
 
-# todo
+cookies <- scan("c:\\temp\\cookies.txt")
+
+t.test(cookies, mu = 2, conf.level = .9, alternative = "greater")
+## p-value = 0.3497 -> 0-Hypothese behalten
+
+# Wie kann ich hier das confidence intervall interpretieren?
+
+# Antwort:
+# Bei einem Konfidenzlevel von 90% kann die Behauptung des Herstellers
+# von einem maximalen Anteil von 2g beibehalten werden.
+
+
+# Aufgabe: Zweiseitiger Test bei µ, σ unbekannt
+# ----------------------------------------------------------------------------------------------------
+penguins <- scan("C:\\temp\\penguins.txt")
+
+# Da fehlt was. Aufgabe kann nicht gelöst werden. Zur Bestimmung einer Veränderung brauchen wir
+# eine Grundgrösse.
+
+# Aufgabe: Linksseitiger Test des Populationsanteils p
+# ----------------------------------------------------------------------------------------------------
+
+### Was ist hier die 0-Hypothese
+
+grocery <- read.csv("C:\\temp\\grocerystore.csv", sep = ";")
+grocery_sex <- grocery$gender
+n <- length(grocery_sex)
+k <- length(grocery_sex[grocery_sex == "F"])
+
+prop.test(k, n, p = .5, conf.level = .95, alternative = "less", correct = FALSE)
+# p-value = 0.9938 -> 0-Hypothese verwerfen
+
+# Antwort:
+# Aufgrund der Stichprobe lässt sich bei einem Konfidenzlevel von 95%
+# behaupten, dass die Metzgerei zu einem grösseren Teil von Frauen besucht wird.
+
+
+# Aufgabe: Rechtsseitiger Test des Populationsanteils p
+# ----------------------------------------------------------------------------------------------------
+
+library(data.table)
+credit_csv <- read.csv("C:\\temp\\creditcards.csv", sep = ";")
+credits <- credit_csv %>% as.data.table()
+number_of_credits <- nrow(credits)
+number_of_ruptured_credits <- nrow(credits[bounced == "Yes"])
+
+prop.test(number_of_ruptured_credits, number_of_credits, alternative = "greater",
+          p = .12, conf.level = .99, correct = FALSE)
+## p-value = 0.02581 -> 0-Hypothese muss behalten werden
+
+# Antwort:
+# Die Bank kann bei einer Konfidenz von 99% davon ausgehen, dass der Anteil
+# von geplatzten Krediten 12% nicht übersteigt
+
+
+# Aufgabe: Zweiseitiger Test des Populationsanteils p
+# ----------------------------------------------------------------------------------------------------
+
+# Hier ist aus meiner Sicht die Frage nicht korrekt:
+# Hier müsste sich die Schätzung doch auf alle Studierenden und nicht auf die Stichprobe beziehen
+
+library(MASS)
+hands <- survey %>%  as.data.table()
+total_hands <- nrow(hands)
+right_hands <- nrow(hands[W.Hnd == "Right",])
+
+prop.test(right_hands, total_hands, p = .9, conf.level = .99,
+          alternative = "two.sided", correct = FALSE)
+## p-value = 0.3088 -> 0-Hypothese wird behalten
+
+# Antwort:
+# Die Schätzung von 90% Rechtshänder kann bei einer Konfidenz von 99%
+# beibehalten werden.
+
