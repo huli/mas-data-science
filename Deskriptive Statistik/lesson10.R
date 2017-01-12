bf8e5d0fa16331e9ee06e754d12fa810508a12d4
Huli <huli@eitschei.com>
2017-01-12 10:57:49 +0100

    Regression added.

--- Deskriptive Statistik/lesson10.R
+++ Deskriptive Statistik/lesson10.R
@@ -18,3 +18,73 @@ chisq.test(ab)
 
 # Zur Kontrolle noch Grenze bestimmen
 qchisq(.95, df = 4)
+
+
+# Testen einer Verteilungsform / Anpassungstest
+# Hinterfragung von Verteilungen
+
+# Beispiel 39
+
+# Überprüfung auf einem Signifikanzniveau α = 0,05, ob das Gewicht von
+# Zuckerpaketen nicht normalverteilt ist.
+# Stichprobe: n = 100, x 999,93 = , s2 = 0,25
+# Beobachtete relative Häufigkeiten (zur Vereinfachung der Darstellung nur
+#                                    4 Intervalle):
+#   Intervall relative Häufigkeit pib
+        # unter 999,5       0,2
+        # [999,5; 1.000]    0,32
+        # [1.000; 1.000,5]  0,34
+        # über 1.000,5      0,14
+
+
+mu <- 999.93
+
+# Welcher Anteil ist kleiner als 999.5
+pnorm(999.5, mean = mu, sd = .5)
+## [1] 0.1948945
+
+pnorm(1000, mean = mu, sd = .5) -
+  pnorm(999.5, mean = mu, sd = .5)
+## [1] 0.3607755
+
+pnorm(1000.5, mean = mu, sd = .5) -
+  pnorm(1000, mean = mu, sd = .5)
+## [1] 0.3171868
+
+pnorm(1000.5, mean = mu, sd = .5, lower.tail = F)
+## [1] 0.1271432
+
+tab <- matrix(c(.2, .32, .34, .14, .1949, .3608, .3172, .1271), nrow = 2, byrow = T)
+
+# Achtung, nun in Stuecken
+chisq.test(tab[1,] * 100, p = tab[2,])
+
+# oder
+chisq.test(c(20, 32, 34, 14), p = c(.1949, .3608, .3172, .1271))
+
+## So testet auch Steuerverwaltung auf Verteilung von zahlen
+## https://en.wikipedia.org/wiki/Benford's_law
+
+# Testen von Hypothesen über einen statistischen Zusammenhang
+# zweier metrischer Merkmale
+
+# Aufgabe: Fundierte Entscheidung zwischen zwei konkurrierenden 
+# Unterstellungen über den Zusammenhang zweier metrischer Merkmale
+
+# Again: lateinische Buchstaben für Stichproben, griechische für Population
+
+# Beschreibende Statistik
+cor(faithful$eruptions, faithful$waiting)
+
+# Wir betrachten jetzt aber eine Stichhprobe und wollen Aussagen 
+# über die Grundgesammtheit machen
+
+cor.test(faithful$eruptions, faithful$waiting)
+# -> H0 wird verworfen
+
+# Für nicht metrische Werte
+# method = c("pearson", "kendall", "spearman"),
+
+plot(faithful$waiting, faithful$eruptions)
+abline(lm(faithful$eruptions ~ faithful$waiting))
+
