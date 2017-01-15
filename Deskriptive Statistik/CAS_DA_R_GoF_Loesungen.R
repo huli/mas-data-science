ba6da74e9a37e657b40eab8d511b662314abeca3
Huli <huli@eitschei.com>
2017-01-15 22:52:20 +0100

    Exercises added.

---
+++ Deskriptive Statistik/CAS_DA_R_GoF_Loesungen.R
@@ -0,0 +1,12 @@
+# ----------------------------------------------------------------------------------------------------
+# Modul:      Deskriptive Statistik
+# Author:     Christoph Hilty
+# Datum:      15.01.2017
+# Aufgaben:   CAS_DA_R_GoF_Aufgaben.pdf 
+# ----------------------------------------------------------------------------------------------------
+
+# Aufgabe: Anpassungstests
+
+
+
+# Aufgabe: Unabhängigkeitstests
