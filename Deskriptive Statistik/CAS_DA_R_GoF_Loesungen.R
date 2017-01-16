b7623a43768f45faf66bb3d6f230c603dc08db1e
Huli <huli@eitschei.com>
2017-01-16 22:38:18 +0100

    Exercise added.

--- Deskriptive Statistik/CAS_DA_R_GoF_Loesungen.R
+++ Deskriptive Statistik/CAS_DA_R_GoF_Loesungen.R
@@ -5,8 +5,46 @@
 # Aufgaben:   CAS_DA_R_GoF_Aufgaben.pdf 
 # ----------------------------------------------------------------------------------------------------
 
+library(MASS)
+
 # Aufgabe: Anpassungstests
 
+## Wieso wird hier lower.tail = F genommen????
+
+# H0: Das Rauchverhalten ist wie angenommen
+# Ha: Das Rauchverhalten ist nicht wie angenommen
+
+n <- length(na.omit(survey$Smoke))
+actual <- table(survey$Smoke)
+expected <- c(.045, .795, .085, .075) * n
+
+# Chi2 berechnen
+chi <- sum((actual - expected)^2/expected)
+## [1] 0.1074429
+
+# p-Wert berechnen
+df <- length(table(survey$Smoke)) -1
+pval <- pchisq(chi, df = df, lower.tail = F)
+## [1] 0.9909295
 
 
 # Aufgabe: Unabhängigkeitstests
+
+# H0: keine Abhängigkeit 
+# Ha: Abhängigkeit
+
+library(openxlsx)
+daten <- read.xlsx("C:\\temp\\RauchenGeschlecht.xlsx")
+
+chisq.test(table(daten$Geschlecht.des.Kindes, 
+                 daten$Rauchverhalten.der.Eltern))
+## X-squared = 2.9511, df = 2, p-value = 0.2287
+## -> p-Value grösser als 0.05 -> wir behalten H0
+
+# oder auch
+chisq.test(daten$Geschlecht.des.Kindes, 
+           daten$Rauchverhalten.der.Eltern)
+
+
+
+
