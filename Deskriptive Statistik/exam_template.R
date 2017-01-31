7417c39988a03598e9f24f14cb15e1291cac828c
Huli <huli@eitschei.com>
2017-01-31 19:55:20 +0100

    Minor changes.

--- Deskriptive Statistik/exam_template.R
+++ Deskriptive Statistik/exam_template.R
@@ -16,9 +16,34 @@ library(ggplot2)
 
 
 
+t.test(Daten_Schulung$Pre, Daten_Schulung$Post, conf.level = .95,
+       correct = F, alternative = "greater")
+
+# todo
+confint(lm(Daten_Wachstum$Wachstumsrate~Daten_Wachstum$Erfahrung), level = .95)
 
 
+summary(lm(Daten_Wachstum$Wachstumsrate~Daten_Wachstum$Erfahrung))
+sqrt(0.3812)
 
+# Merken
+# Paired Test ist mit Differenz
+
+# Merken !!!!!
+t.test(Daten_Wachstum$Alter ~Daten_Wachstum$Geschlecht,
+       conf.level=.95 )
+
+# TODO
+# Varianz, Sd von Anteil -> übersicht ergänzen
+
+# ü 62
+prop.test(102, 1200, p = .10, correct = F,
+          conf.level = .95, alternative = "less")
+
+# Ü 54
+SE <- sqrt(.666*(1-.666)/100)
+E <- qt(.975, 99) * SE
+.666 + c(-E,E)
 
 
 # Stichprobengrösse fuer Anteil ermitteln
