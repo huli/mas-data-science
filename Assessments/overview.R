5ec87b15d596ce5a7db77fb08df35c915c47206d
Huli <huli@eitschei.com>
2017-03-08 15:12:27 +0100

    Boxplot added.

--- Assessments/overview.R
+++ Assessments/overview.R
@@ -15,6 +15,7 @@ assessments_df %>%
   dplyr::mutate(minutes_per_test = as.numeric(minutes)/as.numeric(tests_done)) -> 
   assessments_eval
 
+
 assessments_eval %>% 
   ggplot(aes(factor(applicant), tests_per_minute*10, fill=applicant)) +
   geom_bar(stat = "identity") +
@@ -28,7 +29,19 @@ assessments_eval %>%
             color="darkgray", size=3)  ->
   overview
 
-overview 
+overview
+
+assessments_eval %>% 
+  ggplot(aes(tests_per_minute*10, x="applicants")) + 
+  geom_boxplot(colour="#006d2c", fill="#99d8c9")  +
+  theme(axis.title.x=element_blank(),axis.title.y=element_blank()) ->
+  boxplot
+
+boxplot
+
+library(gridExtra)
+grid.arrange(overview, boxplot, ncol=2)
+
 
 pdf("C:/Source/mas-data-science/Assessments/overview.pdf")
 overview
