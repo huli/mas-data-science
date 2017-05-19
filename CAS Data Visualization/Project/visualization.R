5ee012bca5d5ec0b793f445576fd97ae6b355937
Huli <huli@eitschei.com>
2017-05-19 20:58:54 +0200

    Some stats added.

--- CAS Data Visualization/Project/visualization.R
+++ CAS Data Visualization/Project/visualization.R
@@ -5,8 +5,19 @@ library(dplyr)
 
 
 # Bubble Chart with HDI vs EF per Capita
-country_metrics <- 
-  read_csv("C:/Source/mas-data-science/CAS Data Visualization/Project/country_metrics.csv")
+country_metrics <- read_csv(
+  paste0("C:/Source/mas-data-science/CAS Data Visualization/Project/",
+         "country_metrics.csv"))
+
+# Trying to analyze Mongolia (why is the footprint that high)
+# country_extended_metrics <- read_csv(
+#   paste0("C:/Source/mas-data-science/CAS Data Visualization/Project/",
+#          "NFA 2017 Edition.csv"))
+# country_extended_metrics %>%
+#   filter(year == 2013) %>%
+#   filter(country == "Mongolia" | country == "Switzerland") %>%
+#   filter(record == "BiocapPerCap") %>%
+#   select(country, total)
 
 country_deltas <- read_csv(
   paste0("C:/Source/mas-data-science/CAS Data Visualization/Project/",
@@ -30,4 +41,21 @@ country_metrics_with_impact %>%
             hjust = -.1)+
   ylab("Ecological foodprint per person")+
   ggtitle("World Foodprints and human developments")
+
+limit <- 10
+
+# Countries with highest EF
+# (Mongolia is interesting - what does it miss?)
+country_metrics_with_impact %>% 
+  select(Country, EFConsPerCap) %>% 
+  arrange(desc(EFConsPerCap)) %>% 
+  head(10)
+
+# Countries with lowest EF
+# (Mongolia is interesting - what does it miss?)
+country_metrics_with_impact %>% 
+  select(Country, EFConsPerCap) %>% 
+  arrange(EFConsPerCap) %>% 
+  head(10)
+
 
